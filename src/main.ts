6bccbb8cfb07d405b14639bd2a88604db2595cf7
Duc (Dusk) M. LE <dmle@gosecure.net>
2022-07-22 13:46:37 +0700

    Update authenticated.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import * as session from 'express-session';
 import {
   CorsConfig,
   NestConfig,
